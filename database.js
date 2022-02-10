ea7e69321a4f25d992e5762a0a199eadf75b59dd
MarcianoKamala <99209168+MarcianoKamala@users.noreply.github.com>
2022-02-10 13:53:46 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    dialect : 'postgres',
+    host: 'localhost',
+    username: 'postegres',
+    password: '1992',
+    database: 'projetofinal',
+
+    define:{
+        timestamp: true,
+        underscored: true,
+        undefinedAll:true,
+    }
+}
